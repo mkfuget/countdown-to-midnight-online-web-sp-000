542af8407ebfe1df66a3b077a524f297816d45ed
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 06:51:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,10 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
+countdown_with_sleep(number)  
+while(number > 0)
+    puts "#{number} SECOND(S)!"
+    number-=1;
+  end
+  return "HAPPY NEW YEAR!"
+end
